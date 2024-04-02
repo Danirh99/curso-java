8289fda46f4a96fb87c4594d43571f5aefe94eb5
Daniel Ruiz <danicreations698@gmail.com>
2024-04-02 16:36:22 +0200

    Primera Clase

--- src/1 -Tu Primer Hola Mundo en Java/HolaMundo.java
+++ src/1 -Tu Primer Hola Mundo en Java/HolaMundo.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class HolaMundo {
 
     public static void main(String[] args) {
 
+        // Hola Mundo Básico
         System.out.println("Hola Mundo");
+
+        // Hola Mundo con entrada de usuario
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduce tu nombre: ");
+        String nombre = scanner.nextLine();
+
+        System.out.println("Hola Mundo, mi nombre es " + nombre);
+
+        // Hola Mundo con formato especial
+        System.out.printf("%s %s %s", "Hola", "Mundo", "!!!");
 
     }
 
